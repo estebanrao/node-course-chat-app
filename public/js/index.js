e6c0669ff450ec30db8ce6cbc4e4ead7c2490251
Esteban Rao <estebanrao@gmail.com>
2018-12-02 23:12:48 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Pepa',
-        text: 'Chupame un huevo'
-    });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Pepe',
-        text: 'Hola, la concha de tu hermana',
-        createdAt: 123
-    });
-
     socket.on('createMessage', message => {
         console.log('createEmail', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime(),
+        });
     });
 
     socket.on('disconnect', () => {
